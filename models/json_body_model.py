adb7d531bdfb5cc92d02c497bc35fe7529847268
VeraTsaryk <120461488+VeraTsaryk@users.noreply.github.com>
2023-11-20 23:43:15 +0200

    clear code

--- models/json_body_model.py
+++ models/json_body_model.py
@@ -1,12 +1,10 @@
 Post_body_comment_test = {
-
     "id": 3,
     "body": "some ",
     "postId": 5
 }
 
 Put_body_comment_test = {
-
     "id": 1,
     "body": "Vera ",
     "postId": 5
@@ -16,7 +14,6 @@ Post_body_post_test = {
     "id": 4,
     "title": "Post 4"
 }
-
 
 Put_body_post_test = {
     "id": 1,
